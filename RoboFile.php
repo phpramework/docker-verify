fafcd8226bfaface4f0d366902cbba8cd5fe4ae7
phpramework <phpramework@gmail.com>
2017-02-09 03:43:13 +0700

    PHPDbg throwing up some strange error.

--- RoboFile.php
+++ RoboFile.php
@@ -5,7 +5,7 @@ final class RoboFile extends \Robo\Tasks
     public function verify($opts = ['debug|d' => false])
     {
         $debug = !$opts['debug'] ? '' : ' --debug';
-        $result = $this->_exec('phpdbg -qrr /usr/local/bin/codecept run --fail-fast'.$debug);
+        $result = $this->_exec('codecept run --fail-fast'.$debug);
         file_put_contents('/result/verify.output', $result->getOutputData());
 
         if (! $result->wasSuccessful()) {
